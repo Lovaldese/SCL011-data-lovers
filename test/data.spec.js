e966e5b254e0d25657a62239ad0ec978668bea52
Mariajose <mariajosegomezcampo@gmail.com>
2019-09-23 17:39:59 -0400

    test listos

--- test/data.spec.js
+++ test/data.spec.js
@@ -9,18 +9,22 @@ describe('species', () => {
    const show = [
  
   { 
+    
     "name": "Rick Sanchez",
+    "status": "Alive",
     "species": "Human",
 
    },
 
    {
     "name": "Aqua Morty",
+    "status": "unknown",
     "species": "Humanoid",
   },
 
    { 
     "name": "Alien Morty",
+    "status": "unknown",
     "species": "Alien",
    },
 
@@ -31,7 +35,17 @@ describe('species', () => {
 
   it('debería retornar "Rick Sanchez" al filtrar por species "Human"', () => {
     assert.deepEqual(window.filter.species(show,"Human"), [{"name": "Rick Sanchez",
-    "species": "Human",}]);
+    "status": "Alive","species": "Human",}]);
 
   });
-})
+
+  it('debería ser una función', () => {
+    assert.equal(typeof window.filter.species, 'function');
+  });
+  it('debería retornar "Aqua Morty" al filtrar por species "Humanoid"', () => {
+    assert.deepEqual(window.filter.species(show,"Humanoid"), [{"name": "Aqua Morty",
+    "status": "unknown","species": "Humanoid",}]);
+  
+});
+
+});
