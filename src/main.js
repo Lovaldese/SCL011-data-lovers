fef6af67998642cdc79bb24e13eea7eb820dd850
Mariajose <mariajosegomezcampo@gmail.com>
2019-09-22 21:14:19 -0400

    filtrado,imprime en pantalla el filtrado,boton de inicio funciona,tarjetas ya realizadasy carga los personajes al hacer click en personajes

--- src/main.js
+++ src/main.js
@@ -1,69 +1,73 @@
-
-/* Manejo del DOM *///--->todos loe ventos en donde el usuario interactua, llamando a las funciones de data.js
-//1.guardamos elementos para poder trabajar( guardar data)
-//2.Nuestra primera historia de usuario es mostrar todos los personajes desde un inicio(nombre e imagen)AL HACER CLICK EN PERSONAJES
-
+// Manejo del DOM 
+///--->todos loe ventos en donde el usuario interactua, llamando a las funciones de data.js
+// //1.guardamos elementos para poder trabajar( guardar data)
+// //2.Nuestra primera historia de usuario es mostrar todos los personajes desde un inicio(nombre e imagen)AL HACER CLICK EN PERSONAJES
+//data completa 
 const data = window.RICKANDMORTY.results;
+//data filtrada
+// const species= window.filter.species(data,filterSelector);
 
+//Crear variables para los contenedores 
+  const contenedorPadre = document.getElementById("root");
+  const contenedorPersonajes = document.getElementById("printScreenCharacters")
 
-
-
-
- 
-
-//Crear un contenedor padre para root donde se va imprimir lo que yo quiera
-const contenedor_padre = document.getElementById("root");
-
-// //Creando funcionalidad del boton de inico
-//  document.getElementById("Inicio").addEventListener("click", () =>{
-//  document.getElementById("Personajes").style.display="none";
-//  document.getElementById("temporadas").style.display="none";
-//  document.getElementById("Inicio").style.display="block";
-
-// })
-
-//FUNCION MOSTRAR
-//1. guardamos el botòn mostrar de html, lo guardamos en una variable
-//const personajes = document.getElementById("personajes");
-//2.hacemos que el boton escuche el click
-// imprimi el listado ordenado
-
- document.getElementById("personaje").addEventListener("click", ()=>{
- let persoResult = window.filter.personajes(data);
-
- console.log(persoResult);
-
-
-  })
-
-//FUNCION FILTRAR
-//  asigno a una variable el id del select
-//le asigno un evento al select
+// asignar variables a mis botones y select
+ const myStart = document.getElementById("start")
+const myCharacters = document.getElementById("characters");
+// const myChapters = document.getElementById("chapters");
+// const myFunFacts = document.getElementById("fun facts");
 const filterSpecies = document.getElementById("filterSelector");
-console.log(filterSpecies)
-filterSpecies.addEventListener("change",() =>{
-  // document.getElementById("root").innerHTML = " ";
-
- 
-condition = document.getElementById(filterSpecies);//asigno la condicion y me imprime el valor que escoja el usuario
-let filterSelector= document.getElementById("filterSelector").value
-  speciesResult = window.filter.species(data,filterSelector);
 
 
-   for(let i = 0; i < data.length; i++){
-      document.getElementById("root").innerHTML += `
-   <div>
-  <h4><b> ${data[i].name}</b></h4>
-   <img src="${data[i].image}" alt="image">
-   <h5>${data[i].species}</h5>
-   </div>
+// crear las tarjetas 
+function cards(data){
+  data.forEach(show=>{
+  contenedorPersonajes.innerHTML += 
+     `<div class= "showcharacters">
+       <img src="${show.image}" alt="">
+       <h3>${show.name}</h3
+             <p>${show.species}</p>
+        </div>`
+    });  //cierre forEach
+  
+  } //cierre funcion cards
 
+// Crear una funcion que me contenga las tarjetas con la propiedad que esta en mi data
+// //  MOSTRAR LA DATA COMPLETA AL HACER CLICK EN PERSONAJES
+// hacemos que el boton escuche el click
+myCharacters.addEventListener("click", ()=>{
+  contenedorPersonajes.innerHTML = " ";
 
-      `;
+cards(data); // llamo la fincion de mi tarjeta y le indico el parametro
 
-   }
+});  
 
+myStart.addEventListener("click", ()=>{
+  // contenedorPadre.innerHTML =" ";
+contenedorPersonajes.style.display= "none";
+
+});
+
+ //FUNCION FILTRAR
+// 1 Le asigno un evento al select
+// 2 defino mi condicion,que sera el valor que seleccione el usuario en el select 
+
+ filterSpecies.addEventListener('change', () => {
+ let condition = filterSpecies.options[filterSpecies.selectedIndex].value;
+  let speciesResult = window.filter.species(data,condition);
+//  console.log(speciesResult)
+ contenedorPersonajes.innerHTML = " ";
+cards(speciesResult);
  });
+
+
+   
+
+
+
+
+
+
 
 
 
